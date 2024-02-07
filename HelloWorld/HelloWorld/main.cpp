04925a5a65414f86c36f292a4061f1f0de567398
Avi <you@example.com>
2024-02-07 20:06:26 +0200

    Apply texture

--- HelloWorld/HelloWorld/main.cpp
+++ HelloWorld/HelloWorld/main.cpp
@@ -2,10 +2,12 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include "shader.h"
+#include "stb_image.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 void processInput(GLFWwindow* window);
+unsigned int createTexture();
 
 // settings
 const unsigned int SCR_WIDTH = 800;
@@ -55,12 +57,15 @@ int main()
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
+    // first 3 - 3D vertices
+    // next 3 - vertex color
+    // next 2 - texture coordinates
     float vertices[] = {
-        0.0f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f,  // triangle top  
-        -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // triangle/square left  
-        0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // triangle/square right  
-        0.5f,  0.5f, 0.0f, 0.25f, 0.25f, 0.25f,  // square top right
-        -0.5f,  0.5f, 0.0f, 0.75f, 0.75f, 0.75f   // square top left 
+        0.0f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.5f, 1.0f,  // triangle top  
+        -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, // triangle/square left  
+        0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, // triangle/square right  
+        0.5f,  0.5f, 0.0f, 0.25f, 0.25f, 0.25f, 1.0f, 1.0f,  // square top right
+        -0.5f,  0.5f, 0.0f, 0.75f, 0.75f, 0.75f, 0.0f, 1.0f,  // square top left 
     };
 
     unsigned int indices[] = {  // note that we start from 0!
@@ -69,7 +74,7 @@ int main()
         2, 1, 4    // second triangle
     };
 
-    unsigned int VBO, VAO, EBO;
+    unsigned int VBO, VAO, EBO, texture;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
     glGenBuffers(1, &EBO);
@@ -82,14 +87,19 @@ int main()
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
 
     // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
     glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    texture = createTexture();
 
     // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
     // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
@@ -115,6 +125,7 @@ int main()
         // draw our first triangle
         ourShader.use();
 
+        glBindTexture(GL_TEXTURE_2D, texture);
         glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
 
         //glDrawArrays(GL_TRIANGLES, 0, 3);
@@ -175,4 +186,35 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
     {
         isTriangle = !isTriangle;
     }
+}
+
+unsigned int createTexture() {
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    // set the texture wrapping/filtering options (on the currently bound texture object)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    // load and generate the texture
+    int width, height, nrChannels;
+    unsigned char* data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+        return -1;
+    }
+
+    stbi_image_free(data);
+
+    return texture;
 }
